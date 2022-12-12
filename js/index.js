c4123d22b406cc35ad74691d455c7f21cf4d8389
+stephen.nene <+>
2022-12-12 09:56:51 +0300

    modified:   style/style.css

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ fetch('https://stephen-nene.github.io/WK3-CodeChallenge/db.json')
 .then(res=>(res.json()))
 .then(data=>{
   data =data.films
-    const side = document.querySelector('div.mySideNav')
+    const side = document.querySelector('div.sidenav')
     print (side)
 
 
