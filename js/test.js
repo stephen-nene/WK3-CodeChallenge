29b947aeaca020dee37608b39b95701fbeb5f080
+stephen.nene <+>
2022-12-16 09:54:35 +0300

    fixed reduce ticket number button

--- js/test.js
+++ js/test.js
@@ -4,22 +4,17 @@ print =  (value) => {console.log(value)};
 
 // reduce the number of tickets 
 const btn = document.getElementById('buy-ticket')
-print(btn)
-
 btn.addEventListener('click', function(e){
     let number = document.querySelector('#number').textContent
     e.preventDefault()
     print(number)
     if(number > 0){
-        document.querySelector('#number').textContent  = number-1
-        
+        document.querySelector('#number').textContent  = number-1 
     }
     else {
         alert("sold out,try again earlier next time") 
-        btn.textContent = 'Sold Out'
         btn.className = "btn btn-danger"
         btn.classList.add('')
-        document.querySelector('#number').textContent = "sold-out"
-    }
-})
-print(btn.className)
+        document.querySelector('#available-ticket').textContent = "sold-out"
+}})
+
